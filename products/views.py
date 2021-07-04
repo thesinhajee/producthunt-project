7a70153a144e3cda170f32858654abc922c40948
Aditya Sinha <adityasinha2001@gmail.com>
2021-07-05 01:30:53 +0530

    Finished product-hunt clone

--- products/views.py
+++ products/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Product
 from django.utils import timezone
 # Create your views here.
 def home(request):
-    return render(request, 'products/home.html')
+    products=Product.objects
+    return render(request, 'products/home.html',{'products':products})
 
-@login_required
+@login_required(login_url="/accounts/signup")
 def create(request):
     if request.method=='POST':
         if request.POST['title'] and request.POST['url'] and request.POST['body'] and request.FILES['icon'] and request.FILES['image']:
@@ -22,8 +23,20 @@ def create(request):
             product.date=timezone.datetime.now()
             product.player=request.user
             product.save()
-            return redirect('home')
+            return redirect('/products/'+str(product.id))
         else:
             return render(request, 'products/create.html', {'error':'Filling all the required fields is compulsory'})
     else:
         return render(request, 'products/create.html')
+
+def detail(request, product_id):
+    product=get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/detail.html', {'product':product})
+
+@login_required(login_url="/accounts/signup")
+def upvote(request, product_id):
+    if request.method=='POST':
+        product=get_object_or_404(Product, pk=product_id)
+        product.vote=product.vote+1
+        product.save()
+        return redirect('/products/'+str(product.id))
